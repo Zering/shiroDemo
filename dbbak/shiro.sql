67c9edd473397941c6e62b629acfd5c3d131ba3f
unknown <zhanghaojie@bojoy.net>
2017-04-14 18:07:48 +0800

    db信息 便于用其他机器继续

---
+++ dbbak/shiro.sql
@@ -0,0 +1,143 @@
+/*
+SQLyog Ultimate v11.24 (64 bit)
+MySQL - 5.6.16 : Database - shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shiro` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `shiro`;
+
+/*Table structure for table `roles_permissions` */
+
+DROP TABLE IF EXISTS `roles_permissions`;
+
+CREATE TABLE `roles_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(100) DEFAULT NULL,
+  `permission` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_roles_permissions` (`role_name`,`permission`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `roles_permissions` */
+
+insert  into `roles_permissions`(`id`,`role_name`,`permission`) values (1,'role1','+user1+10'),(3,'role1','+user2+10'),(2,'role1','user1:*'),(4,'role1','user2:*');
+
+/*Table structure for table `sys_permissions` */
+
+DROP TABLE IF EXISTS `sys_permissions`;
+
+CREATE TABLE `sys_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `available` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sys_permissions_permission` (`permission`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_permissions` */
+
+/*Table structure for table `sys_roles` */
+
+DROP TABLE IF EXISTS `sys_roles`;
+
+CREATE TABLE `sys_roles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `available` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sys_roles_role` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_roles` */
+
+/*Table structure for table `sys_roles_permissions` */
+
+DROP TABLE IF EXISTS `sys_roles_permissions`;
+
+CREATE TABLE `sys_roles_permissions` (
+  `role_id` bigint(20) NOT NULL DEFAULT '0',
+  `permission_id` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`role_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_roles_permissions` */
+
+/*Table structure for table `sys_users` */
+
+DROP TABLE IF EXISTS `sys_users`;
+
+CREATE TABLE `sys_users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `salt` varchar(100) DEFAULT NULL,
+  `locked` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sys_users_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_users` */
+
+insert  into `sys_users`(`id`,`username`,`password`,`salt`,`locked`) values (1,'zhang','9ae84486a3c43a8a75189cb48d2b9f15','01e460ce9932394a9d762214b9147a91',0);
+
+/*Table structure for table `sys_users_roles` */
+
+DROP TABLE IF EXISTS `sys_users_roles`;
+
+CREATE TABLE `sys_users_roles` (
+  `user_id` bigint(20) NOT NULL DEFAULT '0',
+  `role_id` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_users_roles` */
+
+/*Table structure for table `user_roles` */
+
+DROP TABLE IF EXISTS `user_roles`;
+
+CREATE TABLE `user_roles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `role_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_user_roles` (`username`,`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_roles` */
+
+insert  into `user_roles`(`id`,`username`,`role_name`) values (1,'zhang','role1'),(2,'zhang','role2');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `password_salt` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_users_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`password_salt`) values (3,'zhang','123',NULL),(4,'liu','059e6cc0cb8898950e0a275e7cbe401f','1b889a90cb18e42a3cbf4b3ba8768e9b');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
